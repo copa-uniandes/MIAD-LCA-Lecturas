4218ebca7c16f5202e7f6d925b37477dc54df1ab
MantiMantilla <MantiMantilla@users.noreply.github.com>
2022-03-25 06:42:30 +0000

    deploy: dbe493cddb9a5004fc6ddf0439a167a124e4edcc

---
+++ _nuxt/static/1648190549/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMIAD-LCA-Lecturas\u002F_nuxt\u002Fstatic\u002F1648190549",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FMIAD-LCA-Lecturas\u002F",assetsPath:"\u002FMIAD-LCA-Lecturas\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"17c4bcc4"}}}}(null));
